41719edaa16015b98f4ca30083e325abc34b938f
AylonGitHub <77498622+AylonGitHub@users.noreply.github.com>
2021-01-17 17:38:15 -0300

    Desenvolvimento do FrontEnd
    FrontEnd da tela de login e formaluário de cadastro.

---
+++ cadastrar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Login</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+<div id="corpo-form">
+    <h1>Cadastrar</h1>
+    <form method="Post" action="processa.php">
+    
+     <input type="text" placeholder="Nome Completo">
+     <input type="text" placeholder="Telefone">
+    <input type="email" placeholder="Usuário">
+    <input type="password" placeholder="Senha">
+    <input type="password" placeholder="Confirmar Senha">
+    <input type="submit" value="CADASTRAR">
+  
+    
+    
+    
+    </form>
+    
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Login</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+<div id="corpo-form">
+    <h1>Entrar</h1>
+    <form method="Post" action="processa.php">
+
+    <input type="email" placeholder="Usuário">
+    <input type="password" placeholder="Senha">
+    <input type="submit" value="ACESSAR">
+    <a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se!</strong></a>
+    
+    
+    
+    </form>
+    
+</div>
+</body>
+</html>
